4e6dc13d7c93d816e42ed64695c12948398e7826
ameralday <151586398+ameralday@users.noreply.github.com>
2025-02-04 15:51:56 -0500

    Add html Add js files

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("free the people")
+
+function getComputerChoice() {
+    /*
+
+    //variable for result
+    let result = 0
+
+    variable for math.random?
+
+    create a button that activates the function
+
+    create a random number generator and multiply it by 100 before rounding down so that it returns a whole number (math.floor)
+
+    Have it return the string "rock" when the number is <3 (that will give 3 chances: 0,1,2)
+
+    Have it return the string "paper" when the number is >=3 but also <6 so that it will give 3 chances (3,4,5)
+
+    Have it return the string "scissors" when the number is <9 (also 3 chances: 6,7,8)
+
+    console.log the result
+    
+    
+    */
+    return Math.floor(Math.random() * 10);
+
+
+    
+
+}
+
+console.log(getComputerChoice());
+
